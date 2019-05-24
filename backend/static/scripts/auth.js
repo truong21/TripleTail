a5f9aeb482a4a39b28794bcf1171b51bbb089068
stak21 <stakashima21@gmail.com>
2019-05-24 19:39:19 +0000

    new method to signing in to github

--- backend/static/scripts/auth.js
+++ backend/static/scripts/auth.js
@@ -6,20 +6,30 @@ const login = () => {
 	$('.signin .github-signin').click(github_oauth());
 }
 const github_oauth = ()=> {
-  // Requests an api, which retrieves the auth token
- let github_auth_url = `https://github.com/login/oauth/authorize`
+  // // Requests an api, which retrieves the auth token
+ // let github_auth_url = 'https://github.com/login/oauth/authorize'
 
-  $.ajax({
-    url: github_auth_url,
-	crossDomain: true,
-	dataType: "jsonp",
-    data: {
-		"client_id": "926a308eec433f17e3ff",
-		"scope": {"user": "email"}
-	},
-    success: function (data, textStatus, jQxhr) {
-		alert("hi")
-      }
+  // $.ajax({
+    // url: github_auth_url,
+	// crossDomain: true,
+	// contentType: "application/json",
+	// dataType: "jsonp",
+    // data: {
+		// "client_id": "926a308eec433f17e3ff",
+		// "scope": {"user": "email"}
+	// },
+    // success: function (data, textStatus, jQxhr) {
+		// alert("hi")
+      // }
+	// })
+	$('.signin .github-signin').on('click', function() {
+		OAuth.initialize('gZeK0rjdjMpH70JACRM_kaKLUIc');
+		// Use popup for OAuth
+		OAuth.popup('github').then(github => {
+			console.log(github);
+			console.log(github.me());
+		});
 	})
 }
+
 document.addEventListener('DOMContentLoaded', ready);
