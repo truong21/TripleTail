54bf99a55dd166733882ab64c0c7a7e7e3f095e1
stak21 <stakashima21@gmail.com>
2019-05-24 19:03:29 +0000

    feat(github oauth): Added a handle for the github callback

--- backend/app.py
+++ backend/app.py
@@ -6,13 +6,21 @@ from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS, cross_origin
 from user import User
 
+import requests
+
 # Flask setup
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 host = '127.0.0.1'
+host = '0.0.0.0'
 
 # Cross-Origin Resource sharing
 cors = CORS(app, resouces={r"/*": {"origins": "*"}})
+
+authorization_base_url = 'https://github.com/login/oauth/authorize'
+token_url = 'https://github.com/login/oauth/access_token'
+request_url = 'https://api.github.com'
+
 
 def page_not_found(e):
     """404 error page for nonexistent routes"""
@@ -56,3 +64,31 @@ if __name__ == "__main__":
     app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
     app.register_error_handler(404, page_not_found)
     app.run(host=host)
+
+@app.route('/callback', methods=['GET', 'POST'])
+def handle_callback():
+  '''
+    This function helps exchange temporary 'code' value with a permanent
+    access_token.
+  '''
+
+  if 'code' in request.args:
+    #return jsonify(code=request.args.get('code'))
+    payload = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'code': request.args['code']
+    }
+    headers = {'Accept': 'application/json'}
+    req = requests.post(token_url, params=payload, headers=headers)
+    resp = req.json()
+
+    if 'access_token' in resp:
+        user_info = User(resp['access_token'])
+        return jsonify(access_token=resp['access_token'])
+        #return redirect(url_for('index'))
+    else:
+        return jsonify(error="Error retrieving access_token"), 404
+  else:
+    return jsonify(error="404_no_code"), 404
+
