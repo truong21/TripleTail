ee85c275b736406262ddbf9f5a4f1f6a89390200
jozsa <allisonjoyweiner@gmail.com>
2019-05-24 03:40:59 +0000

    Feature: get the percentage of repos that have a README

--- backend/user.py
+++ backend/user.py
@@ -9,17 +9,21 @@ import requests
 
 class User:
     """a User class"""
+    url = 'https://api.github.com'
+
     def __init__(self, *args, **kwargs):
         """instantiation of User class object"""
         if kwargs:
             self.__set_attributes(kwargs)
         self.user_dict = {}
 
+    @property
     def __set_attributes(self, attr_dict):
         """sets attributes for a User object"""
         for attr, val in attr_dict.items():
             setattr(self, attr, val)
 
+    @property
     def user_dict(self):
         """creates dictionary of user's info"""
         usern = self.username
@@ -35,4 +39,15 @@ class User:
         """returns number of followers of a user"""
         return self.user_dict['followers']
 
-    def 
+    @property
+    def readme_pct(self):
+        """Returns the percentage of repos that have a README"""
+        repo_list = requests.get('{}/users/{}/repos'.format(url, self.username), auth=(self.username, self.password))
+        readme_count = 0
+        for repo in repo_list:
+            response = requests.get('{}/repos/{}/{}/readme'.format(url, self.username, repo.get('name')), auth=(self.username, self.password))
+            if response.status_code == 404:
+                pass
+            else:
+                readme_count += 1
+        return '{:.1%}'.format(readme_count/self.user_dict.get('public_repos'))
