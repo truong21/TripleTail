c4dff6c89ff1e06715e9ab92c3541baf2e970022
stak21 <stakashima21@gmail.com>
2019-05-24 20:39:21 +0000

    click event is now a simple redirect

--- backend/static/scripts/auth.js
+++ backend/static/scripts/auth.js
@@ -4,32 +4,11 @@ const ready = () => {
 
 const login = () => {
 	$('.signin .github-signin').click(github_oauth());
+	alert("hi")
 }
 const github_oauth = ()=> {
-  // // Requests an api, which retrieves the auth token
- // let github_auth_url = 'https://github.com/login/oauth/authorize'
-
-  // $.ajax({
-    // url: github_auth_url,
-	// crossDomain: true,
-	// contentType: "application/json",
-	// dataType: "jsonp",
-    // data: {
-		// "client_id": "926a308eec433f17e3ff",
-		// "scope": {"user": "email"}
-	// },
-    // success: function (data, textStatus, jQxhr) {
-		// alert("hi")
-      // }
-	// })
-	$('.signin .github-signin').on('click', function() {
-		OAuth.initialize('gZeK0rjdjMpH70JACRM_kaKLUIc');
-		// Use popup for OAuth
-		OAuth.popup('github').then(github => {
-			console.log(github);
-			console.log(github.me());
-		});
-	})
+	// Requests an api, which retrieves the auth token
+	window.location.href = "https://github.com/login/oauth/authorize?client_id=926a308eec433f17e3ff"
 }
 
 document.addEventListener('DOMContentLoaded', ready);
