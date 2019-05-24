7fa91e45486fa0a491df26ac3bebd1727b62d945
stak21 <stakashima21@gmail.com>
2019-05-24 18:24:57 +0000

    Added: a login and oauth authenticator

--- backend/static/scripts/auth.js
+++ backend/static/scripts/auth.js
@@ -1,2 +1,25 @@
-console.log('hi');
-$('.github-signin').on('click', () => console.log('HIII'));
+const ready = () => {
+	login()
+}
+
+const login = () => {
+	$('.signin .github-signin').click(github_oauth());
+}
+const github_oauth = ()=> {
+  // Requests an api, which retrieves the auth token
+ let github_auth_url = `https://github.com/login/oauth/authorize`
+
+  $.ajax({
+    url: github_auth_url,
+	crossDomain: true,
+	dataType: "jsonp",
+    data: {
+		"client_id": "926a308eec433f17e3ff",
+		"scope": {"user": "email"}
+	},
+    success: function (data, textStatus, jQxhr) {
+		alert("hi")
+      }
+	})
+}
+document.addEventListener('DOMContentLoaded', ready);
